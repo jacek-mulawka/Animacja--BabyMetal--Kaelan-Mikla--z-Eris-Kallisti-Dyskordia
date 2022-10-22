2bffdeeadf5bb468f74a5708ba02146459c4d1e9
Jacek Mulawka <78687840+jacek-mulawka@users.noreply.github.com>
2022-10-22 23:31:13 +0200

    Add files via upload

---
+++ Source/Animacja__BabyMetal__Kælan_Mikla__pr.dpr
@@ -0,0 +1,19 @@
+﻿program Animacja__BabyMetal__Kælan_Mikla__pr;
+
+uses
+  Vcl.Forms,
+  Animacja__BabyMetal__Kælan_Mikla in 'Animacja__BabyMetal__Kælan_Mikla.pas' {Animacja__BabyMetal__Kaelan_Mikla__Form};
+
+{$R *.res}
+
+begin
+
+  //???ReportMemoryLeaksOnShutdown := DebugHook <> 0;
+
+  Application.Initialize();
+  Application.MainFormOnTaskbar := True;
+  Application.HintHidePause := 30000;
+  Application.CreateForm(TAnimacja__BabyMetal__Kaelan_Mikla__Form, Animacja__BabyMetal__Kaelan_Mikla__Form);
+  Application.Run();
+
+end.
